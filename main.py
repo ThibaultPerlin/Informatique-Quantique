488092cbe6bd07a66102b6ce952b6fbbbefaa934
ThibaultPerlin <103318328+ThibaultPerlin@users.noreply.github.com>
2025-03-14 11:15:54 +0100

    Informatique-Quantique-Exo1.py

--- main.py
+++ main.py
@@ -1,10 +1,49 @@
-from qiskit import QuantumCircuit, transpile, Aer, execute
+from qiskit import QuantumCircuit
+from qiskit.quantum_info import SparsePauliOp
+from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+from qiskit_ibm_runtime import EstimatorV2 as Estimator
+from qiskit_ibm_runtime import QiskitRuntimeService
 
+
+from qiskit_ibm_runtime import QiskitRuntimeService
+
+service = QiskitRuntimeService(
+    channel='ibm_quantum',
+    instance='ibm-q/open/main',
+    token='9a0d14036c614f4525693ab15bb1628e4de27584e41bad2327de6857da4f6124016221c8dcb3e444500dc9268167845b38ec756d2a7d9ee5ac05bcffcfce2dba'
+)
+
+# Or save your credentials on disk.
+# QiskitRuntimeService.save_account(channel='ibm_quantum', instance='ibm-q/open/main', token='ca7bb6eb3d2db46747ff2a775c3e8082065805127b2ad7763d134d7079ce32d377d410a383d0fdd3d3b3902dab08a9f63000c66712cf28a16fd28e0166aa397d')
+
+# Create a new circuit with two qubits
 qc = QuantumCircuit(2)
+ 
+# Add a Hadamard gate to qubit 0
 qc.h(0)
+ 
+# Perform a controlled-X gate on qubit 1, controlled by qubit 0
 qc.cx(0, 1)
+ 
+# Return a drawing of the circuit using MatPlotLib ("mpl"). This is the
+# last line of the cell, so the drawing appears in the cell output.
+# Remove the "mpl" argument to get a text drawing.
+qc.draw("mpl")
 
-simulator = Aer.get_backend('statevector_simulator')
-result = execute(qc, simulator).result()
+# Set up six different observables.
+ 
+observables_labels = ["IZ", "IX", "ZI", "XI", "ZZ", "XX"]
+observables = [SparsePauliOp(label) for label in observables_labels]
 
-print(result.get_statevector())
+
+ 
+service = QiskitRuntimeService()
+ 
+backend = service.least_busy(simulator=False, operational=True)
+ 
+# Convert to an ISA circuit and layout-mapped observables.
+pm = generate_preset_pass_manager(backend=backend, optimization_level=1)
+isa_circuit = pm.run(qc)
+ 
+isa_circuit.draw("mpl", idle_wires=False)
+print('Hello')
